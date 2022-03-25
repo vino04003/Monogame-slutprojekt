8740d18fbf9853fba87c7671809d35540744fae4
vino04003 <89254939+vino04003@users.noreply.github.com>
2022-03-25 15:27:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ny_försök_monogmae
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
